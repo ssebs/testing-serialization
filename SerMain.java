ac4715e5dd34a2034a9d477ec840345e38b041eb
Sebastian Safari <ssebs@ymail.com>
2015-12-22 17:05:22 +0000

    object saving done

--- SerMain.java
+++ SerMain.java
@@ -1,6 +1,7 @@
 
 import java.io.Serializable;
 import java.io.*;
+import java.net.*;
 
 class SerMain
 {
@@ -13,24 +14,74 @@ class SerMain
         // https://github.com/ssebs/testing-serialization/raw/master/temp2.txt
         
         try{
-              String datums = URLDataHelper.getStringDataAsString("https://raw.githubusercontent.com/ssebs/ssebsEngine/master/ssebs%20Engine/Misc/serverTestFile.txt");
-      System.out.println(datums);
-      FileOutputStream fos = new FileOutputStream("temp2.txt");
-    	ObjectOutputStream oos = new ObjectOutputStream(fos);
-    	Dog d = new Dog("Dog2", 36, 4);
-    	oos.writeObject(d);
-    	oos.flush();
-    	oos.close();
+           // String datums = URLDataHelper.getStringDataAsString("https://raw.githubusercontent.com/ssebs/ssebsEngine/master/ssebs%20Engine/Misc/serverTestFile.txt");
+         //   System.out.println(datums);
+            
+        //    FileInputStream fin = new FileInputStream("temp2.out"); LOCAL FILE
+            InputStream fin = new URL("https://github.com/ssebs/testing-serialization/raw/master/temp2.txt").openStream(); // URL
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            Dog dawg =  (Dog)ois.readObject();
+            ois.close();
+            System.out.println(dawg.toString());
     
         }catch(Exception e)
         {
             e.printStackTrace();
         }
      
+ 
     }
+ 
+/**
+ * returns a Dog object from a URL from the internet 
+ */
+ private Dog readObject(String url)
+ {
+     Dog dawg;
+     try {
+          InputStream fin = new URL(url).openStream(); // URL
+            ObjectInputStream ois = new ObjectInputStream(fin);
+             dawg = (Dog) ois.readObject();
+            ois.close();
+     } catch(Exception e) {
+     }
+     return dawg;
+ }
+ 
+ /**
+ * returns a Dog object from a local file
+ */
+ private Dog readObject(String path)
+ {
+     Dog dawg;
+     try {
+          InputStream fin = new FileInputStream(path); //local file
+            ObjectInputStream ois = new ObjectInputStream(fin);
+             dawg = (Dog) ois.readObject();
+            ois.close();
+     } catch(Exception e) {
+     }
+     return dawg;
+ }
+ /**
+  * saves a Dog object to a path
+  */
+ private void saveObject(Dog d, String path)
+ {
+    try {
+        FileOutputStream fos = new FileOutputStream(path);
+    	ObjectOutputStream oos = new ObjectOutputStream(fos);
+    	oos.writeObject(d);
+    	oos.flush();
+    	oos.close();
+    } catch(Exception e) {
+    }
+ }
+ 
+ 
     
 }
-class Dog  implements Serializable{
+class Dog implements Serializable{
     
     int age;
     int height;
